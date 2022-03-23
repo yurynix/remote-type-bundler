25d8bb9661e3c4035c4436aed6f68e844fc5e0c5
Yury Michurin <yurym@wix.com>
2022-03-23 16:54:21 +0200

    Report version in CLI

--- bin.js
+++ bin.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 const { bundle } = require('./dist/bundle');
+const path = require('path');
 
 (async function main() {
-    console.log(`input: ${process.argv[2]} output: ${process.argv[3]}`);
+    const myPkgJson = require(path.join(__dirname, 'package.json'));
+
+    console.log(`${myPkgJson.name}@${myPkgJson.version} input: ${process.argv[2]} output: ${process.argv[3]}`);
+
+    if (process.argv.length < 4) {
+        console.log(`Too few arguments, can not proceed.`);
+        process.exit(1);
+    }
+
     try {
         await bundle(process.argv[2], process.argv[3]);
     } catch(ex) {
         console.error(`Failed to process ${process.argv[2]} : ${ex.message}`);
-        process.exit(1);s
+        process.exit(1);
     }
 })();
