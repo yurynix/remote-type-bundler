5ba618d50bef9cceacc36e1ef7a2b82573928262
Yury Michurin <yurym@wix.com>
2022-03-23 15:29:40 +0200

    Adjust API

--- bin.js
+++ bin.js
@@ -2,6 +2,11 @@
 const { bundle } = require('./dist/bundle');
 
 (async function main() {
-    console.log(`input: ${process.argv[2]} output: ${process.argv[3]}`)
-    await bundle(process.argv[2], process.argv[3]);
+    console.log(`input: ${process.argv[2]} output: ${process.argv[3]}`);
+    try {
+        await bundle(process.argv[2], process.argv[3]);
+    } catch(ex) {
+        console.error(`Failed to process ${process.argv[2]} : ${ex.message}`);
+        process.exit(1);s
+    }
 })();
